92a6ec9c6d31094377d89a95eeada22fc9e29b35
anaclarauhren07 <143837945+anaclarauhren07@users.noreply.github.com>
2024-10-01 16:16:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Nutrição',
+    'Em que a Nutrição Comportamental pode ajudar?',
+    'melhorar a relação com a comida e incentivar metas realistas'
+)
+
+criaCartao(
+    'Saúde e Bem-Estar',
+    'Qual a importancia de manter a hidratação do corpo?',
+    'A hidratação é essencial para o funcionamento adequado do corpo'
+)
+
+criaCartao(
+    'Nutrição',
+    'Qual a importancia de incluir uma variedade de alimentos saudáveis?',
+    'Manter o corpo saudável e seguir uma dieta'
+)
+
+criaCartao(
+    'Saúde e Bem-Estar',
+    'Qual a quantidade certa para beber água?',
+    'Varia de acordo com idade, estatura, nível de attividade física. mas o recomendado é de 35ml para cada kilo do corpo.'
+)
